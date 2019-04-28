7ce39d943ba88365ad80a22ef3cf03928988ea90
gregfour51 <gregfour51@gmail.com>
2019-04-28 14:50:58 -0700

    Update hello.pl
    revise script

--- hello.pl
+++ hello.pl
@@ -1,10 +1,8 @@
 #!C:\stawberry\perl\bin\perl.exe -w
 
-$a = "world", "greg";
-
-$i=0;
-
-while($i<2) {
-print ("Hello, $a_[$i]!\n");
-	$i++;
-}
+use strict;
+use warnings;
+use CGI;
+use CGI::Carp('fatalsToBrowser');
+print "Content-type: text/html \n\n";
+print "Hello World.\n";
